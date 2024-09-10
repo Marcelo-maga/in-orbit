bbb8370d424f078024f1a1369c11ef4c809df511
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-09-10 11:12:27 -0300

    feat: add route create goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,9 +1,39 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../services/create-goal'
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log("HTTP server is running")
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async ({ body }) => {
+    await createGoal({
+      title: body.title,
+      desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+    })
+  }
+)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server is running')
+  })
