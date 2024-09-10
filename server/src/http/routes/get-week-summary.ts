61e35fd5cc91f48287a216a7ed5a7fe1229fe8a2
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-09-10 13:42:43 -0300

    feat: create summary route

---
+++ server/src/http/routes/get-week-summary.ts
@@ -0,0 +1,8 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getWeekSummary } from '../../services/get-week-summary'
+
+export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/summary', async () => {
+    return await getWeekSummary()
+  })
+}
