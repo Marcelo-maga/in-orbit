e857f91e72d29cb1f510dc8395303bca0e9c8fc4
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-09-13 12:22:28 -0300

    feat: create form to create new goal

--- apps/web/src/components/create-goal.tsx
+++ apps/web/src/components/create-goal.tsx
@@ -11,9 +11,42 @@ import {
 } from "@nextui-org/react";
 import { CustomRadio } from "./radio-custom";
 import { Plus } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createGoal } from "../http/create-goal";
+import { useQueryClient } from "@tanstack/react-query";
+
+const createGoalForm = z.object({
+  title: z.string().min(1, "Informe a atividade"),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+});
+
+type CreateGoalFormType = z.infer<typeof createGoalForm>;
 
 export function CreateGoal() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const queryClient = useQueryClient();
+
+  const { register, handleSubmit, reset, control, formState } =
+    useForm<CreateGoalFormType>({
+      resolver: zodResolver(createGoalForm),
+    });
+
+  async function onSubmit(data: CreateGoalFormType) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    });
+
+    queryClient.invalidateQueries({
+      queryKey: ["get-pending-goals"],
+    });
+
+    queryClient.invalidateQueries({
+      queryKey: ["get-summary"],
+    });
+  }
 
   return (
     <>
@@ -42,70 +75,93 @@ export function CreateGoal() {
                       Qual atividade?
                     </label>
                     <Input
+                      {...register("title")}
                       autoFocus
                       placeholder="Praticar exercícios, meditar, etc..."
+                      isInvalid={!!formState.errors.title}
+                      errorMessage={formState.errors.title?.message}
                     />
                   </div>
 
-                  <RadioGroup
-                    className="w-full"
-                    label="Quantas vezes na semana?"
-                  >
-                    <CustomRadio
-                      description="Comece devagar... mas com estilo!"
-                      value="1"
-                    >
-                      1x na semana
-                    </CustomRadio>
+                  <Controller
+                    control={control}
+                    name="desiredWeeklyFrequency"
+                    defaultValue={1}
+                    render={({ field }) => {
+                      return (
+                        <RadioGroup
+                          value={String(field.value)}
+                          onValueChange={field.onChange}
+                          className="w-full"
+                          label="Quantas vezes na semana?"
+                        >
+                          <CustomRadio
+                            description="Comece devagar... mas com estilo!"
+                            value="1"
+                          >
+                            1x na semana
+                          </CustomRadio>
 
-                    <CustomRadio
-                      description="Duas vezes é só o começo!"
-                      value="2"
-                    >
-                      2x na semana
-                    </CustomRadio>
+                          <CustomRadio
+                            description="Duas vezes é só o começo!"
+                            value="2"
+                          >
+                            2x na semana
+                          </CustomRadio>
 
-                    <CustomRadio
-                      description="Três momentos para fazer acontecer!"
-                      value="3"
-                    >
-                      3x na semana
-                    </CustomRadio>
+                          <CustomRadio
+                            description="Três momentos para fazer acontecer!"
+                            value="3"
+                          >
+                            3x na semana
+                          </CustomRadio>
 
-                    <CustomRadio
-                      description="Quatro chances para arrasar!"
-                      value="4"
-                    >
-                      4x na semana
-                    </CustomRadio>
+                          <CustomRadio
+                            description="Quatro chances para arrasar!"
+                            value="4"
+                          >
+                            4x na semana
+                          </CustomRadio>
 
-                    <CustomRadio
-                      description="Cinco chances para conquistar!"
-                      value="5"
-                    >
-                      5x na semana
-                    </CustomRadio>
-                    <CustomRadio
-                      description="Seis dias de ação, um de descanso!"
-                      value="6"
-                    >
-                      6x na semana
-                    </CustomRadio>
+                          <CustomRadio
+                            description="Cinco chances para conquistar!"
+                            value="5"
+                          >
+                            5x na semana
+                          </CustomRadio>
 
-                    <CustomRadio
-                      description="Todo dia é dia de brilhar!"
-                      value="7"
-                    >
-                      Todos os dias da semana
-                    </CustomRadio>
-                  </RadioGroup>
+                          <CustomRadio
+                            description="Seis dias de ação, um de descanso!"
+                            value="6"
+                          >
+                            6x na semana
+                          </CustomRadio>
+
+                          <CustomRadio
+                            description="Todo dia é dia de brilhar!"
+                            value="7"
+                          >
+                            Todos os dias da semana
+                          </CustomRadio>
+                        </RadioGroup>
+                      );
+                    }}
+                  />
                 </form>
               </ModalBody>
               <ModalFooter>
                 <Button className="flex-1" color="default" onPress={onClose}>
                   Fechar
                 </Button>
-                <Button className="flex-1" color="primary" onPress={onClose}>
+                <Button
+                  className="flex-1"
+                  color="primary"
+                  onClick={handleSubmit(async (data) => {
+                    await onSubmit(data);
+                    onClose();
+                    reset();
+                  })}
+                >
                   Salvar
                 </Button>
               </ModalFooter>
